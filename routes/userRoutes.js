ff9c0b3b6e186bb067fb5cbb5bec6f9960b6f34e
iamlive <71422116+iamcrypto@users.noreply.github.com>
2024-11-16 15:05:21 +0530

    commit it
    it

--- routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const { registerUser, loginUser } = require('../conrollers/userController');
-
-const router = require('express').Router();
-
-// /users/register
-router.post('/register', registerUser);
-router.post('/login', loginUser)
-
-module.exports = router;
